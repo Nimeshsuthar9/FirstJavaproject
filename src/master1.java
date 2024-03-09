b8ba55fa0d49cc19f609eb47ea962faba4fd6a99
Nimeshkumar Suthar <162324572+Nimeshsuthar9@users.noreply.github.com>
2024-03-09 15:11:48 +0000

    Change as per comment

--- src/master1.java
+++ src/master1.java
@@ -4,6 +4,6 @@ public class master1
 {
     public static void main(String[] args)
     {
-        System.out.println("Welcome To My World");
+        System.out.println("Welcome To Git");
     }
 }
